a1f1d18f568560eb736df60e67a2a0b3249f816f
kingflamez <flamekeed@gmail.com>
2018-05-16 09:24:01 +0100

    storeID

--- rave.php
+++ rave.php
@@ -36,12 +36,12 @@ function aes_128_decrypt($key, $data)
 // Get payload from the POST and process it
 $ecwid_payload = $_POST['data'];
 $client_secret = "coLPrs9NN5mDgr6xqwUaNAF0PAqBF3Zr"; // this is a dummy value. Please place your app secret key here
-
+header('Content-Type: application/json');
 // The resulting JSON array will be in $result variable
 $result = getEcwidPayload($client_secret, $ecwid_payload);
 $json_string = json_encode($result, JSON_PRETTY_PRINT);
 $object = json_decode(json_encode($result), false);
-echo $object->storeId;
+echo $json_string;
 die();
 
 ?>
